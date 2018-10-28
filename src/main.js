d82f7497a2bc653cacc3fe05b574a62a5a732ffb
Barış Gündüz <baris.gunduz7@gmail.com>
2018-10-28 17:54:40 +0300

    Handling auth state change

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import firebase from 'firebase/app'
 import store from './store'
+import auth from 'firebase/auth'
 
 Vue.config.productionTip = false
 
@@ -21,11 +22,17 @@ firebase.initializeApp(config);
 
 window.firebase = firebase;
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  store,
-  components: { App },
-  template: '<App/>'
+const unsubscribe = firebase.auth().onAuthStateChanged(user => {
+  // dispatch user
+  store.dispatch('setUser', user)
+
+  new Vue({
+    el: '#app',
+    router,
+    store,
+    components: { App },
+    template: '<App/>'
+  });
+  // recursion - this function calls itself on auth state change
+  unsubscribe()
 })
